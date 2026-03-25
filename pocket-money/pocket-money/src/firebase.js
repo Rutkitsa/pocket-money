4cc13a54d0634d3f063e5b8673407597baa08574
Rutkitsa <100295485+Rutkitsa@users.noreply.github.com>
2026-03-25 15:19:59 +0200

    Add files via upload

---
+++ pocket-money/pocket-money/src/firebase.js
@@ -0,0 +1,18 @@
+// 🔧 הכנס כאן את פרטי ה-Firebase שלך
+// ראה הוראות בקובץ README.md
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyBMNDG8zdy1VMCn6ARHf2hp4xrEIWO5uD8",
+  authDomain:        "pocket-money-d754e.firebaseapp.com",
+  databaseURL:       "https://pocket-money-d754e-default-rtdb.firebaseio.com",
+  projectId:         "pocket-money-d754e",
+  storageBucket:     "pocket-money-754e.firebasestorage.app",
+  messagingSenderId: "164163551777",
+  appId:             "1:164163551777:web:f609ae1fe4b0911cf61960",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
